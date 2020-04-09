45e7bd1d7a893f76b4fc67704e20918b1448879c
Nelson Reina (@nelreina) <nelreina@gmail.com>
2020-04-08 20:25:43 -0400

    just a test program

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 // Generated project cognito
 require('dotenv').config();
 const log4js = require('@nelreina/node-log4js');
-    
+const http = require('http');
+const express = require('@nelreina/node-express');
 const argv = require('minimist')(process.argv.slice(2));
+
 const logger = log4js('cognito');
 const DEBUG = process.env['DEBUG'];
 
-logger.info(`Start Project cognito: ${JSON.stringify({DEBUG, argv})}  `)
+const PORT = process.env['PORT'] || 8080;
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send({ message: 'Hey I am here' })
+})
+
+app.post('/', (req, res) => {
+  try {
+    const data = req.body;
+    logger.debug(JSON.stringify(data));
+    res.send(data)
+
+  } catch (error) {
+    logger.error(error.message);
+    res.status(503).send(error.message)
+  }
+})
+
+const server = http.createServer(app);
+server.listen(PORT, () => logger.info(`API is running on port ${PORT}`));
+
+logger.info(`Start Project cognito: ${JSON.stringify({ DEBUG, argv })}  `)
